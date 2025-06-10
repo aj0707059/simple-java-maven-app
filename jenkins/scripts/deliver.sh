e64571ccebe05d83121e21c02f34f94c1f7bdf3a
aj0707059 <aj0707059@gmail.com>
2025-06-10 17:58:56 +0530

    Update deliver.sh
    checking current path

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -22,5 +22,7 @@ set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+current_path=$(pwd)
+echo "Current path: $current_path"
 set -x
 java -jar /var/jenkins_home/workspace/GitHubPipeline/target/${NAME}-${VERSION}.jar
